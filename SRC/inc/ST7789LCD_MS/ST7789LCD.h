aefefa1023c90417cbc66098be5e0fe31a4e9456
Shreyas <skombinadka@gmail.com>
2026-09-18 23:16:16 +0530

    Window functions working

--- SRC/inc/ST7789LCD_MS/ST7789LCD.h
+++ SRC/inc/ST7789LCD_MS/ST7789LCD.h
@@ -22,11 +22,11 @@ struct ST7789LCD
     uint16_t HEIGHT;
     uint16_t WIDTH;
 
-    uint16_t ROW_ADDR[4];
-    uint16_t COL_ADDR[4];
+    uint16_t ROW_ADDR[10];
+    uint16_t COL_ADDR[10];
 
     uint8_t BOX_NUM;
-    uint16_t TEXT_BOX[16];
+    uint16_t TEXT_BOX[40];
 };
 
 void st7789lcd_pinset(uint8_t CS, uint8_t CS_PORT, uint8_t DC, uint8_t DC_PORT, uint8_t RST, uint8_t RST_PORT, uint8_t LED, uint8_t LED_PORT); // TFT(ST7789) LCD display pin connections
